ee77402be9d01707569e37c720ddcf75308b3280
Martin <mrscdevel@gmail.com>
2021-10-01 15:58:28 -0400

    make more readable

--- cmd/geth/config_test.go
+++ cmd/geth/config_test.go
@@ -9,53 +9,44 @@ import (
 )
 
 func TestConfigArgs(t *testing.T) {
-	// app created in init()
 	durationArg := "2m11s"
-	duration, err := time.ParseDuration(durationArg)
+
+	// command line
+	args := []string{"", "--http.timeout", durationArg, "--dev"}
+	// app created in init()
+	app.Action = action(t, durationArg, "HTTPRpcTimeout should be "+durationArg)
+	err := app.Run(args)
 	if err != nil {
 		t.Fatal(err)
 	}
 
-	// command line
-	args := []string{"", "--http.timeout", durationArg, "--datadir", ""}
-	app.Action = func(ctx *cli.Context) error {
+	// config file
+	args = []string{"", "--config", "testdata/config.toml", "--dev"}
+	app.Action = action(t, "20s", "HTTPRpcTimeout should 20s")
+	err = app.Run(args)
+
+	// commandline overrides config file
+	args = []string{"", "--http.timeout", durationArg, "--config", "testdata/config.toml", "--dev"}
+	app.Action = action(t, durationArg, "HTTPRpcTimeout should be "+durationArg)
+	err = app.Run(args)
+
+	if err != nil {
+		t.Fatal(err)
+	}
+}
+
+func action(t *testing.T, da, es string) func(ctx *cli.Context) error {
+	d, err := time.ParseDuration(da)
+	if err != nil {
+		t.Fatal(err)
+	}
+	return func(ctx *cli.Context) error {
 		log.Root().SetHandler(log.DiscardHandler())
 		stack, cfg := makeConfigNode(ctx)
-		if duration != cfg.Eth.HTTPRpcTimeout {
-			t.Fatal("HTTPRpcTimeout should be", durationArg)
+		if d != cfg.Eth.HTTPRpcTimeout {
+			t.Fatal(es)
 		}
 		stack.Close() // cleanup db
 		return nil
-	}
-	err = app.Run(args)
-
-	// config file
-	args = []string{"", "--config", "testdata/config.toml", "--datadir", ""}
-	app.Action = func(ctx *cli.Context) error {
-		log.Root().SetHandler(log.DiscardHandler())
-		stack, cfg := makeConfigNode(ctx)
-		if cfg.Eth.HTTPRpcTimeout.Seconds() != 20 {
-			t.Fatal("HTTPRpcTimeout should be 20s")
-		}
-		stack.Close()
-		return nil
-	}
-	err = app.Run(args)
-
-	// command line overrides config file
-	args = []string{"", "--http.timeout", durationArg, "--config", "testdata/config.toml", "--datadir", ""}
-	app.Action = func(ctx *cli.Context) error {
-		log.Root().SetHandler(log.DiscardHandler())
-		stack, cfg := makeConfigNode(ctx)
-		if duration != cfg.Eth.HTTPRpcTimeout {
-			t.Fatal("HTTPRpcTimeout should be", duration)
-		}
-		stack.Close()
-		return nil
-	}
-	err = app.Run(args)
-
-	if err != nil {
-		t.Fatal(err)
 	}
 }
